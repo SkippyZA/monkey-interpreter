a83940ada21de3186955a452088f35ae1a307a26
Steven Inskip <inskips@gmail.com>
2023-02-06 23:23:32 +0100

    parsing simple let statement

---
+++ parser/parser_test.go
@@ -0,0 +1,48 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/skippyza/monkey-interpreter/ast"
+	"github.com/skippyza/monkey-interpreter/lexer"
+	"github.com/skippyza/monkey-interpreter/parser"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+
+	assert.NotNil(t, program)
+	assert.Len(t, program.Statements, 3)
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		testLetStatement(t, stmt, tt.expectedIdentifier)
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) {
+	assert.Equal(t, "let", s.TokenLiteral())
+
+	letStmt, ok := s.(*ast.LetStatement)
+	assert.True(t, ok, "s not *ast.LetStatement. got=%T:", s)
+	assert.Equal(t, letStmt.Name.Value, name)
+	assert.Equal(t, letStmt.Name.TokenLiteral(), name)
+}
